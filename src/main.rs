99c20a78ea92b6712e3ad568649b0a9febf71299
jamshaidtahiri <jamshaid.tahiri1@gmail.com>
2019-10-21 20:30:42 +0500

    initial comit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+#[allow(unused_imports)]
+use aux7::{entry, iprint, iprintln};
+
+#[entry]
+fn main() -> ! {
+    let gpioe = aux7::init().1;
+
+    // Turn on the North LED
+    gpioe.bsrr.write(|w| w.bs9().set_bit());
+
+    // Turn on the East LED
+    gpioe.bsrr.write(|w| w.bs11().set_bit());
+
+    // Turn off the North LED
+    gpioe.bsrr.write(|w| w.br9().set_bit());
+
+    // Turn off the East LED
+    gpioe.bsrr.write(|w| w.br11().set_bit());
+
+    loop {}
 }
+
